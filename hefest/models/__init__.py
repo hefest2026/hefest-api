4db749b41da706029b11654a937321ba699bf8e6
Kirill Ibragimov <hexchap@gmail.com>
2026-06-19 15:39:30 +0300

    feat: add RefreshToken and OAuthIdentity models, make password_hash nullable

--- hefest/models/__init__.py
+++ hefest/models/__init__.py
@@ -1,7 +1,17 @@
 from hefest.models.event import Event
 from hefest.models.notification_job import NotificationJob
 from hefest.models.notification_log import NotificationLog
+from hefest.models.oauth_identity import OAuthIdentity
+from hefest.models.refresh_token import RefreshToken
 from hefest.models.registration import Registration
 from hefest.models.user import User
 
-__all__ = ["User", "Event", "Registration", "NotificationJob", "NotificationLog"]
+__all__ = [
+    "User",
+    "Event",
+    "Registration",
+    "NotificationJob",
+    "NotificationLog",
+    "OAuthIdentity",
+    "RefreshToken",
+]
